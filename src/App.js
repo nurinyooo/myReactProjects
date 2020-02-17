ead963d09de5041f606ab7dde49a8ddab7a8f540
Nurinyooo <nurullahdur@gmail.com>
2020-02-18 00:18:56 +0300

    MyReactProjetcWhichIBegin

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+     <h1>Selam Ben Bir React Projesiyim.</h1>
+    </div>
+  );
+}
+
+export default App;
